2355e1d70bbd174d2700eecdda4d6c4999536116
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-05-30 18:14:32 +0200

    Add overflow

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector('.header_burger');
 const heading = document.querySelector('.heading');
 const headerMenu = document.querySelector('.header_menu');
+const body = document.querySelector('body');
 
 burger.addEventListener('click',() => {
 burger.classList.toggle('active');
 headerMenu.classList.toggle('active');
+body.classList.toggle('lock');
 })
